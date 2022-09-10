088750b8f1bbdf74c67eb0d0c95bf87f2e17d520
Seth Trusky <50874284+Sethtpa@users.noreply.github.com>
2022-09-10 09:57:31 -0400

    Changed ChessBoard

---
+++ ChessBoard.js
@@ -0,0 +1,79 @@
+for(i=i; i < 9; i++){
+    let box1 =document.createElement('div')
+    if(i % 2 == 0){
+        box1.style.backgroungColor = 'black'
+    }  
+    else{
+        box1.style.backgroungColor = 'white'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box2 =document.createElement('div')
+    if(i % 2 == 0){
+        box2.style.backgroungColor = 'white'
+    }  
+    else{
+        box2.style.backgroungColor = 'black'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box3 =document.createElement('div')
+    if(i % 2 == 0){
+        box3.style.backgroungColor = 'black'
+    }  
+    else{
+        box3.style.backgroungColor = 'white'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box4 =document.createElement('div')
+    if(i % 2 == 0){
+        box4.style.backgroungColor = 'white'
+    }  
+    else{
+        box4.style.backgroungColor = 'black'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box5 =document.createElement('div')
+    if(i % 2 == 0){
+        box5.style.backgroungColor = 'black'
+    }  
+    else{
+        box5.style.backgroungColor = 'white'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box6 =document.createElement('div')
+    if(i % 2 == 0){
+        box6.style.backgroungColor = 'white'
+    }  
+    else{
+        box6.style.backgroungColor = 'black'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box7 =document.createElement('div')
+    if(i % 2 == 0){
+        box7.style.backgroungColor = 'black'
+    }  
+    else{
+        box7.style.backgroungColor = 'white'
+    }
+}
+
+for(i=i; i < 9; i++){
+    let box8 =document.createElement('div')
+    if(i % 2 == 0){
+        box8.style.backgroungColor = 'white'
+    }  
+    else{
+        box8.style.backgroungColor = 'black'
+    }
+}
